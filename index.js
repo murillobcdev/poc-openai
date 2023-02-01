02b4fbcc11eee71ff039613e40ffb82342da6b16
murillo <murillobdev@gmail.com>
2023-02-01 16:26:25 -0300

    cors error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -13,10 +13,11 @@ app.use(express.urlencoded({ extended: false }));
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/openai', require('./routes/openaiRoutes'));
+app.use("/openai", require("./routes/openaiRoutes"));
+app.use(cors({
+    origin: 'http://localhost:4200',
+}))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.get("/", (req, res) => console.log("Hello World! Server is running."));
 
 app.listen(port, () => console.log(`Server iniciou na porta: ${port}`));

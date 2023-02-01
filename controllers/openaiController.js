313553dd85fa710dff1e2c0db679f013ed5897b8
murillo <murillobdev@gmail.com>
2023-02-01 17:03:51 -0300

    update environment

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,10 +1,11 @@
+import { environment } from "../environment";
 const { Configuration, OpenAIApi } = require("openai");
 const formidable = require("formidable");
 const fs = require("fs");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: environment.OPENAI_API_KEY,
+  organization: environment.OPENAI_ORGANIZATION,
 });
 
 const openai = new OpenAIApi(configuration);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 // // Set static folder
 
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use("/openai", require("./routes/openaiRoutes"));
 

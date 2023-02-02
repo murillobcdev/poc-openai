53c9ae0ec4e8e27b124ab01e6dda9a35d71fd3c3
murillo <murillobdev@gmail.com>
2023-02-02 18:29:32 -0300

    enable cors

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const port = process.env.PORT || 5000;
 const dotenv = require("dotenv").config();
 const app = express();
 
+//enable cors;
+app.use(cors());
+
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(cors());
 app.use("/openai", require("./routes/openaiRoutes"));
 app.listen(port, () => console.log(`Server iniciou na porta: ${port}`));

4229f9571329c6d02127df9ab00d4a652af4248e
murillo <murillobdev@gmail.com>
2023-02-02 18:39:15 -0300

    cors another try

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const app = express();
 const cors = require("cors");
 
 //enable cors;
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
 // Enable body parser
 app.use(express.json());
